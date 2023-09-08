1883ce22cfa498283482fee5ffaa5b8d687e614d
gusramireles46 <gusramireles46@gmail.com>
2023-09-07 18:33:53 -0600

    Implementación del Alert

--- src/main/java/com/example/tap2023/TAP2023.java
+++ src/main/java/com/example/tap2023/TAP2023.java
@@ -4,21 +4,20 @@ import com.example.tap2023.vistas.Calculadora;
 import com.example.tap2023.vistas.Loteria;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class TAP2023 extends Application {
 
     private Scene escena;
     private BorderPane borderPane;
     private MenuBar menuBar;
-    private Menu menuParcial1, menuParcial2;
-    private MenuItem mniCalculadora, mniLoteria;
+    private Menu menuParcial1, menuParcial2, menuSalir;
+    private MenuItem mniCalculadora, mniLoteria, mniSalir;
 
     private void CrearGUI() {
         // Menú Items
@@ -28,14 +27,30 @@ public class TAP2023 extends Application {
         mniLoteria = new MenuItem("Lotería");
         mniLoteria.setOnAction(event -> new Loteria());
 
+        mniSalir = new MenuItem("Salir");
+        mniSalir.setOnAction(event -> salir());
+
         // Menú
         menuParcial1 = new Menu("Parcial 1");
         menuParcial1.getItems().addAll(mniCalculadora, mniLoteria);
 
         menuParcial2 = new Menu("Parcial 2");
 
+        menuSalir = new Menu("Salir");
+        menuSalir.getItems().add(mniSalir);
+
         // MenuBar
-        menuBar = new MenuBar(menuParcial1, menuParcial2);
+        menuBar = new MenuBar(menuParcial1, menuParcial2, menuSalir);
+    }
+
+    private void salir() {
+        Alert a = new Alert(Alert.AlertType.CONFIRMATION);
+        a.setTitle("Información");
+        a.setHeaderText("Salir");
+        a.setContentText("Estas saliendo del programa");
+        Optional<ButtonType> option = a.showAndWait();
+        if (option.get() == ButtonType.OK)
+            System.exit(0);
     }
 
     @Override
